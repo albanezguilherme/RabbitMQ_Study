cb4faa27dcc094282ab0d25f18e66b05c4124fcd
Guilherme Albanez <g.costa.albanez@gmail.com>
2023-10-08 14:32:13 -0300

    RabbitMQ first steps

---
+++ Sender/Send.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "hello",
+                     durable: false,
+                     exclusive: false,
+                     autoDelete: false,
+                     arguments: null);
+
+
+Console.WriteLine("Type x to exit or type the message to RabbitMQ");
+var stopString = Console.ReadLine();
+
+while (stopString != "x")
+{
+    var body = Encoding.UTF8.GetBytes(stopString);
+
+    channel.BasicPublish(exchange: string.Empty,
+                     routingKey: "hello",
+                     basicProperties: null,
+                     body:  body);
+    Console.WriteLine($" [x] Sent {stopString}");
+
+    stopString = Console.ReadLine();
+}
+
+
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
